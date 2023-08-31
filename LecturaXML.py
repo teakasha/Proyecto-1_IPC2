102c01a61328463b96a22b2461243567681a7729
Akasha <102836783+teakasha@users.noreply.github.com>
2023-08-31 08:44:44 -0600

    se agrego funcionalidad colas

--- LecturaXML.py
+++ LecturaXML.py
@@ -1,10 +1,16 @@
 import xml.etree.ElementTree as ET
 from ListaSimple import ListaSimple
 from  Senal import Senal
+from Pila import Pila
+from Cola import Cola
+import copy
 
 class LecturaXML():
     def __init__(self, path):
         self.raiz = ET.parse(path).getroot()
+        self.pila = Pila()
+        self.lista = ListaSimple()
+        self.cola = Cola()
 
     def getSenal(self):
         listSenales = ListaSimple()
@@ -21,9 +27,25 @@ class LecturaXML():
             print(senalGuardada.getDato().getNombre())
             senalGuardada = senalGuardada.getSiguiente()
 
-    def getDatos():
-        pass
+    def getDatos(self):
+        for senal in self.raiz.findall('senal'):
+            for dato in senal.findall('dato'):
+                self.pila.push(int(dato.text))
+                self.lista.agregarEnOrden(int(dato.text))
+                self.cola.enconlar(int(dato.text))
+
+        pilaBinaria = copy.deepcopy(self.pila)
+        pilaBinaria.convertirABinario()
+        pilaBinaria.graficar('pilaBinaria')
+        self.pila.graficar('pila')
+        self.lista.graficar('lista')
+        self.cola.graficar('cola')
+        self.cola.desencolar()
+        self.cola.graficar('cola2')
+        self.cola.desencolar()
+        self.cola.graficar('cola3')
 
 
-objLectura = LecturaXML('entrada.xml')
-objLectura.getSenal()
+objLectura = LecturaXML('entrada2.xml')
+#objLectura.getSenal()
+objLectura.getDatos()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # IPC2 N
 ## 2do. Semestre 2023
+
+semana6
